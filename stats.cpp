c99dfbc93770fe5a1f3e7d654d9820fa566682cc
Avelar Luna Juan Pablo (RBEI/EGB1-MS) <eva9ga@bosch.com>
2021-10-31 17:44:37 -0600

    we have now compilable code

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,14 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& FloatingPointVector) {
     //Implement statistics here
+    return Stats();
+}
+
+void StatsAlerter::checkAndAlert(std::vector<float> localVector){
+  //Implement
+}
+
+StatsAlerter::StatsAlerter(const float Max_thresh, std::vector<IAlerter*> Alert_objects){
+
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,24 @@
 #include <vector>
 
+struct Stats {
+  float average;
+  float max;
+  float min;
+};
+struct IAlerter{
+  //leave empty
+};
+struct EmailAlert: public IAlerter{
+  bool emailSent = 0;
+};
+struct LEDAlert: public IAlerter{
+  bool ledGlows = 0;
+};
+class StatsAlerter{
+public:
+  void checkAndAlert(std::vector<float> localVector);
+  StatsAlerter(const float Max_thresh, std::vector<IAlerter*> Alert_objects);
+};
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& FloatingPointVector);
 }
