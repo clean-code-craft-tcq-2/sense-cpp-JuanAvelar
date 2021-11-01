51bad9783ba17c1781ef37ea839add1bf6ae73b0
Avelar Luna Juan Pablo (RBEI/EGB1-MS) <eva9ga@bosch.com>
2021-10-31 18:45:20 -0600

    All the structure is now completed, now we still have to make the tests pass and debug

--- stats.cpp
+++ stats.cpp
@@ -6,21 +6,34 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& FloatingPointVecto
     if(!FloatingPointVector.empty()){
       //populate the first number of vector to get rid of initial value NaN.
       stat_instance = {FloatingPointVector.front(),FloatingPointVector.front(),FloatingPointVector.front()};
-      for(auto&& FPV : FloatingPointVector){
+      for(auto&& FPV : FloatingPointVector){//iterate over all vector components
         stat_instance.average += FPV;//Add all values of vector
         if(FPV > stat_instance.max) stat_instance.max = FPV;//get maximum number in vector
         if(FPV < stat_instance.min) stat_instance.min = FPV;//get minimum number in vector
       }
       stat_instance.average = stat_instance.average / FloatingPointVector.size();
     }
-
     return stat_instance;//return by copy
 }
 
-void StatsAlerter::checkAndAlert(std::vector<float> localVector){
-  //Implement
+StatsAlerter::StatsAlerter(const float Max_thresh, std::vector<IAlerter*> Alert_objects)
+  :_private_max_threshold(Max_thresh), _private_alert_objects(Alert_objects){
+  //Implementation not needed, initializer list used instead
 }
 
-StatsAlerter::StatsAlerter(const float Max_thresh, std::vector<IAlerter*> Alert_objects){
-  //Implement
+void StatsAlerter::checkAndAlert(std::vector<float> localVector){
+  //Implement alerts here
+  auto computedStats = Statistics::ComputeStatistics(localVector);
+  if(computedStats.max > _private_max_threshold){
+    for(auto&& SingleAlert : _private_alert_objects){
+      SingleAlert->please_alert();
+    }
+  }
+}
+
+void EmailAlert::please_alert(void){
+  emailSent = true;
+}
+void LEDAlert::please_alert(void){
+  ledGlows = true;
 }
--- stats.h
+++ stats.h
@@ -7,18 +7,23 @@ struct Stats {
   float min = NAN;
 };
 struct IAlerter{
-  //leave empty
+  virtual void please_alert(void);
 };
 struct EmailAlert: public IAlerter{
   bool emailSent = false;
+  void please_alert(void) override;
 };
 struct LEDAlert: public IAlerter{
   bool ledGlows = false;
+  void please_alert(void) override;
 };
 class StatsAlerter{
 public:
   void checkAndAlert(std::vector<float> localVector);
   StatsAlerter(const float Max_thresh, std::vector<IAlerter*> Alert_objects);
+private:
+  std::vector<IAlerter*>  _private_alert_objects;
+  const float             _private_max_threshold;
 };
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& FloatingPointVector);
