7076da1b3c88bfa8d1d6fcf04c05171f74af5ec8
Avelar Luna Juan Pablo (RBEI/EGB1-MS) <eva9ga@bosch.com>
2021-10-31 18:54:02 -0600

    initialization of average calculation fixed, Assignment completed

--- stats.cpp
+++ stats.cpp
@@ -1,11 +1,14 @@
 #include "stats.h"
 
+#define INIT_AVRG 0.0
+
 Stats Statistics::ComputeStatistics(const std::vector<float>& FloatingPointVector) {
     //Implement statistics here
     Stats stat_instance;
     if(!FloatingPointVector.empty()){
       //populate the first number of vector to get rid of initial value NaN.
-      stat_instance = {FloatingPointVector.front(),FloatingPointVector.front(),FloatingPointVector.front()};
+      //also initialize average as 0.0
+      stat_instance = {INIT_AVRG,FloatingPointVector.front(),FloatingPointVector.front()};
       for(auto&& FPV : FloatingPointVector){//iterate over all vector components
         stat_instance.average += FPV;//Add all values of vector
         if(FPV > stat_instance.max) stat_instance.max = FPV;//get maximum number in vector
@@ -26,6 +29,7 @@ void StatsAlerter::checkAndAlert(std::vector<float> localVector){
   auto computedStats = Statistics::ComputeStatistics(localVector);
   if(computedStats.max > _private_max_threshold){
     for(auto&& SingleAlert : _private_alert_objects){
+      //it doesn't matter how many alert the vector has, it will turn them all ON
       SingleAlert->please_alert();
     }
   }
